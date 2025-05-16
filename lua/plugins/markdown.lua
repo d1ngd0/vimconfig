de65e57ef6495c0ec88c61eac9bbfd5dae806bc8
Paul Montag <paul-montag@target.com>
2025-05-16 08:58:10 -0500

    Added an angry comment because this took more time from my life than it should have... I am remembering the downsides to vim and the shifting sands that all this bullshit is built on top of.
    Man I am not in a great mood.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,6 +2,11 @@ local HOME = os.getenv("HOME")
 return {
   "mfussenegger/nvim-lint",
   optional = true,
+  -- this removes line linting where it complains if the line is longer than 80 characters
+  -- in fucking markdown
+  -- where we write sentences
+  -- Are you fucking kidding me? the pedantic dipshit that obsesively enforces this on their
+  -- markdown files needs a talking to.
   opts = {
     linters = {
       ["markdownlint-cli2"] = {
